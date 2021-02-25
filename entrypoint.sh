e24edfe86c35b6887779a7ba0b7f119df8434794
Jacqueline Wagner <jacqueline.wagner@dedalus-group.com>
2021-02-25 10:03:10 +0100

    iterate trouhgh subfolders of allure-results

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,13 +13,14 @@ GITHUB_PAGES_WEBSITE_URL="https://${INPUT_GITHUB_REPO_OWNER}.github.io/${REPOSIT
 
 for d in ${INPUT_ALLURE_RESULTS}/*/; do
 
-  SUBFOLDER = ${d:${#INPUT_ALLURE_RESULTS}+1}
+  SUBFOLDER=${d:${#INPUT_ALLURE_RESULTS}+1}
   SUBFOLDER=${SUBFOLDER%/*} # cut slash at the end
 
-  INPUT_ALLURE_HISTORY="${INPUT_ALLURE_HISTORY}/allure-report-${SUBFOLDER}"
+  ALLURE_REPORT="allure-report-${SUBFOLDER}"
+  INPUT_ALLURE_HISTORY="${INPUT_ALLURE_HISTORY}/${ALLURE_REPORT}"
   echo "NEW allure history folder ${INPUT_ALLURE_HISTORY}"
   mkdir -p ./${INPUT_ALLURE_HISTORY}
-  GITHUB_PAGES_WEBSITE_URL="${GITHUB_PAGES_WEBSITE_URL}/${SUBFOLDER}"
+  GITHUB_PAGES_WEBSITE_URL="${GITHUB_PAGES_WEBSITE_URL}/${ALLURE_REPORT}"
   echo "NEW github pages url ${GITHUB_PAGES_WEBSITE_URL}"
 
   #echo "index.html"
@@ -34,26 +35,26 @@ for d in ${INPUT_ALLURE_RESULTS}/*/; do
   echo "\"buildUrl\":\"https://github.com/${INPUT_GITHUB_REPO}/actions/runs/${INPUT_GITHUB_RUN_ID}\"," >>executor.json
   echo "\"buildName\":\"GitHub Actions Run #${INPUT_GITHUB_RUN_ID}\",\"buildOrder\":\"${INPUT_GITHUB_RUN_NUM}\"}" >>executor.json
   #cat executor.json
-  mv ./executor.json ./${INPUT_ALLURE_RESULTS}
+  mv ./executor.json ./${d}
 
   #environment.properties
   echo "URL=${GITHUB_PAGES_WEBSITE_URL}" >environment.properties
-  mv ./environment.properties ./${INPUT_ALLURE_RESULTS}
+  mv ./environment.properties ./${d}
 
-  echo "keep allure history from ${INPUT_ALLURE_HISTORY}/last-history to ${INPUT_ALLURE_RESULTS}/history"
-  cp -r ./${INPUT_ALLURE_HISTORY}/last-history/. ./${INPUT_ALLURE_RESULTS}/history
+  echo "keep allure history from ${INPUT_ALLURE_HISTORY}/last-history to ${d}/history"
+  cp -r ./${INPUT_ALLURE_HISTORY}/last-history/. ./${d}/history
 
   #echo "version ${INPUT_ALLURE_VERSION}"
 
-  echo "generating report from ${INPUT_ALLURE_RESULTS} to ${INPUT_ALLURE_REPORT} ..."
-  #ls -l ${INPUT_ALLURE_RESULTS}
-  allure generate --clean ${INPUT_ALLURE_RESULTS} -o ${INPUT_ALLURE_REPORT}
+  echo "generating report from ${d} to ${ALLURE_REPORT} ..."
+  #ls -l ${dS}
+  allure generate --clean ${d} -o ${ALLURE_REPORT}
   #echo "listing report directory ..."
-  #ls -l ${INPUT_ALLURE_REPORT}
+  #ls -l ${ALLURE_REPORT}
 
   echo "copy allure-report to ${INPUT_ALLURE_HISTORY}/${INPUT_GITHUB_RUN_NUM}"
-  cp -r ./${INPUT_ALLURE_REPORT}/. ./${INPUT_ALLURE_HISTORY}/${INPUT_GITHUB_RUN_NUM}
+  cp -r ./${ALLURE_REPORT}/. ./${INPUT_ALLURE_HISTORY}/${INPUT_GITHUB_RUN_NUM}
   echo "copy allure-report history to /${INPUT_ALLURE_HISTORY}/last-history"
-  cp -r ./${INPUT_ALLURE_REPORT}/history/. ./${INPUT_ALLURE_HISTORY}/last-history
+  cp -r ./${ALLURE_REPORT}/history/. ./${INPUT_ALLURE_HISTORY}/last-history
 
 done
